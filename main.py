0106be471c2851f73c693ec2162e6dd36719b589
Niramay Shrivastava <niramayshri0501@gmail.com>
2025-12-21 22:29:19 +0530

    Restrict CORS to specific origins

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ app = FastAPI(title="MindMate")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["http://localhost:3000",
+        "https://mind-mate-ai-production.up.railway.app"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
